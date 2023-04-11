ca13dad95a56056ae62511ed66412d883fa38a55
kty990 <masterhotdog9@gmail.com>
2023-04-11 00:32:59 -0600

    .

--- src/index.py
+++ src/index.py
@@ -1,1 +1,10 @@
-### CODE HERE
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send('Hello, world!')
+
+bot.run('YOUR_BOT_TOKEN_HERE')
