cf37c41a780359ce45bb74fb70c87982e71b73c2
kty990 <masterhotdog9@gmail.com>
2023-04-10 20:42:14 -0600

    Added Embed class

--- coup/lib/discord.mjs
+++ coup/lib/discord.mjs
@@ -4,7 +4,43 @@ import { assert } from './util.mjs';
 
 class Embed {
     constructor() {
+        this.embed = {
+            title: "\u2800",
+            description: "\u2800",
+            fields: [],
+            author: {
+                name: "embed",
+                icon_url: ""
+            },
+            color: 0xffffff,
+            timestamp: new Date().toISOString(),
+            footer: {
+                text: '\u2800',
+                icon_url: '',
+            },
+        };
+    }
 
+    SetTitle(title = "\u2800") {
+        this.embed.title = title;
+    }
+
+    SetDescription(desc = "\u2800") {
+        this.embed.description = desc;
+    }
+
+    SetColor(hex = 0xffffff) {
+        this.embed.color = hex;
+    }
+
+    SetAuthor(name = "embed", icon_url = "") {
+        this.embed.author.name = name;
+        this.embed.author.icon_url = icon_url;
+    }
+
+    SetFooter(text = "embed", icon_url = "") {
+        this.embed.author.text = text;
+        this.embed.author.icon_url = icon_url;
     }
 }
 
@@ -31,13 +67,23 @@ class DiscordBot {
     }
 
     SendMessage(channel = null, content = "") {
-        assert(channel != null, "Can't send message to null channel.");
+        assert(channel == null, "Can't send message to null channel.");
         if (content instanceof Embed) {
-
+            channel.send({ embeds: content.embed })
+                .catch(console.error);
+        } else {
+            channel.send(content)
+                .catch(console.error);
         }
     }
 
     // WaitForInput(MESSAGE) << Waits for message to be sent
+    async WaitForInput(msg = null) {
+        assert(msg == null, "Can't wait for an empty message");
+        return new Promise((resolve, reject) => {
+            resolve();
+        });
+    }
 
     // WaitForInputFromMember(MEMBER)
 
