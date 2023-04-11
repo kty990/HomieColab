61abefe6ebebcb0d90279fec3f450f95bd4db380
kty990 <masterhotdog9@gmail.com>
2023-04-11 00:39:43 -0600

    .

--- src/index.py
+++ src/index.py
@@ -1,10 +1,29 @@
+import os
 import discord
+import json
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+# Get the absolute path of the auth.json file
+auth_file = os.path.join(os.path.dirname(__file__), 'auth.json')
 
+# Open the auth.json file and load the token
+with open(auth_file, 'r') as f:
+    data = json.load(f)
+    token = data.get('token')
+
+# Create intents object
+intents = discord.Intents.default()
+
+# Create bot instance with intents
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Define a command
 @bot.command(name='hello')
 async def hello(ctx):
     await ctx.send('Hello, world!')
 
-bot.run('YOUR_BOT_TOKEN_HERE')
+# Run the bot
+if token:
+    bot.run(token)
+else:
+    print("Error: Token not found in .auth.json")
