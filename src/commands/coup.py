5839979d3434ba6bba345b6e41e1dc6021fa6cce
kty990 <masterhotdog9@gmail.com>
2023-04-11 15:49:22 -0600

    Modified function name (test -> run)

--- src/commands/coup.py
+++ src/commands/coup.py
@@ -1,2 +1,5 @@
-def test():
-    print("Yes the test works")
+import os
+current_file = os.path.basename(__file__)
+def run(ctx):
+    ### THIS IS EXECUTED WHEN THE COMMAND IS RUN
+    ctx.send("this is placeholder text for the %s command" % (str(current_file).replace(".py","")))
